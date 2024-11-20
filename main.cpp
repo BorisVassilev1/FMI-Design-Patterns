feabea3fe36d6a1568151382831e36d3ac11336a
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-11-20 14:34:46 +0200

    added 'load' command

--- main.cpp
+++ main.cpp
@@ -1,38 +1,15 @@
 #include <iostream>
-#include "src/abstract_factory.hpp"
-#include "src/figure_factory.hpp"
 #include "src/commands.hpp"
 
 int main() {
 	std::string inputMethod;
 
-	AbstractFigureFactory abstractff(std::cin);
-
-	auto types = abstractff.listFactoryTypes();
-	std::cout << "Enter input method: [ " << std::endl;
-	for (const auto &[type, argcnt, desc] : types) {
-		std::cout << "    " << desc << '\n';
-	}
-	std::cout << "]" << std::endl;
-
-	std::unique_ptr<FigureFactory> factory = nullptr;
-	do {
-		try {
-			std::cout << "==> " << std::flush;
-			factory = abstractff.create();
-		} catch (const std::exception &e) { std::cerr << "error: " << e.what() << std::endl; }
-	} while (factory == nullptr);
-	std::size_t figureCount = -1;
 
 	std::vector<std::unique_ptr<Figure>> figures;
 
-	while (true) {
-		try {
-			std::unique_ptr<Figure> f = factory->create();
-			if (f == nullptr) break;
-			figures.push_back(std::move(f));
-		} catch (const std::exception &e) { std::cerr << "error" << e.what() << std::endl; }
-	}
+	auto load = std::make_unique<commands::Command>();
+	load.reset(commands::load());
+	(*load)(figures, std::cin, std::cout);
 
 	std::string				 command;
 	commands::CommandFactory cf(std::cin);
--- src/commands.hpp
+++ src/commands.hpp
@@ -3,6 +3,7 @@
 #include <istream>
 #include <fstream>
 #include <stdexcept>
+#include "abstract_factory.hpp"
 #include "factory.hpp"
 #include "figures.hpp"
 
@@ -26,12 +27,13 @@ static auto list = []() -> Command * {
 
 static auto remove = [](std::string id) -> Command * {
 	std::size_t index = std::stoi(id);
-	return new Command([index](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
-		if (index >= data.size()) { throw std::out_of_range("Index out of range"); }
-		data.erase(data.begin() + index);
-		std::cout << "removed " << index << std::endl;
-		return true;
-	});
+	return new Command(
+		[index](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
+			if (index >= data.size()) { throw std::out_of_range("Index out of range"); }
+			data.erase(data.begin() + index);
+			std::cout << "removed " << index << std::endl;
+			return true;
+		});
 };
 
 static auto erase = [](std::string begin, std::string end) -> Command * {
@@ -48,30 +50,33 @@ static auto erase = [](std::string begin, std::string end) -> Command * {
 
 static auto duplicate = [](std::string id) -> Command * {
 	std::size_t index = std::stoi(id);
-	return new Command([index](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
-		if (index >= data.size()) { throw std::out_of_range("Index out of range"); }
-		data.emplace_back(data[index]->clone());
-		std::cout << "duplicated " << index << std::endl;
-		return true;
-	});
+	return new Command(
+		[index](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
+			if (index >= data.size()) { throw std::out_of_range("Index out of range"); }
+			data.emplace_back(data[index]->clone());
+			std::cout << "duplicated " << index << std::endl;
+			return true;
+		});
 };
 
 static auto store = [](std::string filename) -> Command * {
-	return new Command([filename](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
-		std::ofstream file((filename));
-		if (!file.is_open()) { throw std::runtime_error("Cannot open file"); }
-		for (auto &figure : data) {
-			file << (std::string)*figure << std::endl;
-			if (!file) { throw std::runtime_error("Cannot write to file"); }
-		}
-		file.close();
-		std::cout << "writen figures to \"" << filename << "\"" << std::endl;
-		return true;
-	});
+	return new Command(
+		[filename](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool {
+			std::ofstream file((filename));
+			if (!file.is_open()) { throw std::runtime_error("Cannot open file"); }
+			for (auto &figure : data) {
+				file << (std::string)*figure << std::endl;
+				if (!file) { throw std::runtime_error("Cannot write to file"); }
+			}
+			file.close();
+			std::cout << "writen figures to \"" << filename << "\"" << std::endl;
+			return true;
+		});
 };
 
 static auto exit = []() -> Command * {
-	return new Command([](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool { return false; });
+	return new Command(
+		[](std::vector<std::unique_ptr<Figure>> &data, std::istream &in, std::ostream &out) -> bool { return false; });
 };
 
 static auto help = []() -> Command * {
@@ -92,6 +97,38 @@ static auto clear = []() -> Command * {
 	});
 };
 
+static auto load = []() -> Command * {
+	return new Command([](std::vector<std::unique_ptr<Figure>> &figures, std::istream &in, std::ostream &out) -> bool {
+		AbstractFigureFactory abstractff(std::cin);
+
+		auto types = abstractff.listFactoryTypes();
+		std::cout << "Enter input method: [ " << std::endl;
+		for (const auto &[type, argcnt, desc] : types) {
+			std::cout << "    " << desc << '\n';
+		}
+		std::cout << "]" << std::endl;
+
+		std::unique_ptr<FigureFactory> factory = nullptr;
+		do {
+			try {
+				std::cout << "==> " << std::flush;
+				factory = abstractff.create();
+			} catch (const std::exception &e) { std::cerr << "error: " << e.what() << std::endl; }
+		} while (factory == nullptr);
+		std::size_t figureCount = -1;
+
+		while (true) {
+			try {
+				std::unique_ptr<Figure> f = factory->create();
+				if (f == nullptr) break;
+				figures.push_back(std::move(f));
+			} catch (const std::exception &e) { std::cerr << "error" << e.what() << std::endl; }
+		}
+
+		return true;
+	});
+};
+
 JOB(register_commands, {
 	TypeRegistry::registerConstructor("list", std::function(list), "list :  all figures");
 	TypeRegistry::registerConstructor("remove", std::function(remove), "remove i  : remove the figure at index i");
@@ -103,6 +140,7 @@ JOB(register_commands, {
 	TypeRegistry::registerConstructor("exit", std::function(exit), "exit : exit the shell");
 	TypeRegistry::registerConstructor("help", std::function(help), "help : display help information");
 	TypeRegistry::registerConstructor("clear", std::function(clear), "clear : clears the screen");
+	TypeRegistry::registerConstructor("load", std::function(load), "load : load figures with a custom input method");
 
 	TypeRegistry::registerSuccessor("Command", "list");
 	TypeRegistry::registerSuccessor("Command", "remove");
@@ -112,6 +150,7 @@ JOB(register_commands, {
 	TypeRegistry::registerSuccessor("Command", "exit");
 	TypeRegistry::registerSuccessor("Command", "help");
 	TypeRegistry::registerSuccessor("Command", "clear");
+	TypeRegistry::registerSuccessor("Command", "load");
 	return 0;
 });
 }	  // namespace commands
