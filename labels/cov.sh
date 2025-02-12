7562ebf578fae80c1a3f0ca531bbf46c870dfd7a
Boris Vassilev <boris.n.vassilev@gmail.com>
2025-02-12 14:03:53 +0200

    fixed coverage

--- labels/cov.sh
+++ labels/cov.sh
@@ -1,5 +1,5 @@
 mkdir -p build/coverage
-gcovr -r . --html-details build/coverage/index.html -e ../lib/
+gcovr -r . --html-details build/coverage/index.html -e ../lib/ --gcov-executable 'llvm-cov gcov'
 read -p "open in browser? (y/N): " open
 if [ "$open" != "${open#[yY]}" ]; then
 	xdg-open build/coverage/index.html;
